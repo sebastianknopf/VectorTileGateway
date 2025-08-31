cf9bead322ccf2b52eb0a5b4b9e05425ed1f3929
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-08-31 14:55:02 +0200

    fixed Response type in API and added user agents

--- src/api.py
+++ src/api.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from fastapi.responses import Response, StreamingResponse
+from fastapi.responses import JSONResponse, StreamingResponse
 from io import BytesIO
 from requests import get, post
 from requests import Response as HttpResponse
 
 import transform
+import version
 
 app = FastAPI()
 
@@ -17,44 +18,57 @@ async def tile(zoom: int, x: int, y: int, style: str, width: int = 256, height: 
 
     try:
 
+        # define HTTP helper variables
+        http_headers: dict = {
+            'User-Agent': f"VectorTileProxy/{version.__version__}"
+        }
+        
         # transform ZXY slippy map tile into bounding box
         bbox: tuple[float, float, float, float] = transform.tile_bbox(zoom, x, y)
         bbox_str: str = f"{bbox[0]},{bbox[1]},{bbox[2]},{bbox[3]}"
 
         # fetch desired style
-        style: HttpResponse = get(style)
+        style: HttpResponse = get(
+            style, 
+            headers=http_headers
+        )
 
         # combine all parameters and style to render everything by the
         # vector-tile-renderer
-        response: HttpResponse = post('http://vector-tile-renderer:80/render', json={
-            'style': style.json(),
-            'bounds': bbox_str,
-            'width': width,
-            'height': height
-        })
+        response: HttpResponse = post(
+            'http://vector-tile-renderer:80/render', 
+            json={
+                'style': style.json(),
+                'bounds': bbox_str,
+                'width': width,
+                'height': height
+            },
+            headers=http_headers
+        )
 
         # check status response of vector-tile-renderer
         if response.status_code == 200:
 
             # return rendered tile as PNG file
-            return StreamingResponse(BytesIO(response.content), media_type='image/png')
+            return StreamingResponse(
+                BytesIO(response.content), 
+                media_type='image/png'
+            )
         else:
 
             # return error response
-            return Response(
+            return JSONResponse(
                 status_code=response.status_code, 
                 content={
                     'error': response.content.decode('utf-8')
-                },
-                media_type='application/json'
+                }
             )
 
     except Exception as ex:
         
-        return Response(
+        return JSONResponse(
             status_code=500, 
             content={
                 'error': str(ex)
-            },
-            media_type='application/json'
+            }
         )
