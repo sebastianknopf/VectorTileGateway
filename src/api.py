3796e728e914c166dd385bf3ee928db6cbe602c8
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-08-31 14:45:43 +0200

    improved code for API

--- src/api.py
+++ src/api.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from fastapi.responses import Response, StreamingResponse
 from io import BytesIO
-
-import requests
+from requests import get, post
+from requests import Response as HttpResponse
 
 import transform
 
@@ -15,18 +15,46 @@ async def index():
 @app.get('/tile/{zoom}/{x}/{y}.png')
 async def tile(zoom: int, x: int, y: int, style: str, width: int = 256, height: int = 256):
 
-    style = requests.get(style)
+    try:
 
-    bbox = transform.tile_bbox(zoom, x, y)
+        # transform ZXY slippy map tile into bounding box
+        bbox: tuple[float, float, float, float] = transform.tile_bbox(zoom, x, y)
+        bbox_str: str = f"{bbox[0]},{bbox[1]},{bbox[2]},{bbox[3]}"
 
-    response = requests.post('http://vector-tile-renderer:80/render', json={
-        "style": style,
-        "bounds": f"{bbox[0]},{bbox[1]},{bbox[2]},{bbox[3]}",
-        "width": width,
-        "height": height
-    })
+        # fetch desired style
+        style: HttpResponse = get(style)
 
-    if response.status_code != 200:
-        return {"error": response.content.decode()}
+        # combine all parameters and style to render everything by the
+        # vector-tile-renderer
+        response: HttpResponse = post('http://vector-tile-renderer:80/render', json={
+            'style': style.json(),
+            'bounds': bbox_str,
+            'width': width,
+            'height': height
+        })
 
-    return StreamingResponse(BytesIO(response.content), media_type="image/png")
+        # check status response of vector-tile-renderer
+        if response.status_code == 200:
+
+            # return rendered tile as PNG file
+            return StreamingResponse(BytesIO(response.content), media_type='image/png')
+        else:
+
+            # return error response
+            return Response(
+                status_code=response.status_code, 
+                content={
+                    'error': response.content.decode('utf-8')
+                },
+                media_type='application/json'
+            )
+
+    except Exception as ex:
+        
+        return Response(
+            status_code=500, 
+            content={
+                'error': str(ex)
+            },
+            media_type='application/json'
+        )
